da73feaa87e725138b1c85fa24b0796f5be7c550
Oscar Calaf <ocalaf@gmail.com>
2022-09-22 16:43:30 +0200

    changed package name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(top_level_directory, 'README.md'), encoding='utf-8') as file
     long_description = file.read()
 
 setup(
-    name='nautobot_ui_plugin',
+    name='nautobot_ui_plugin_unitecnic',
     version='0.10.2',
     url='https://gitlab-ce.gwdg.de/gwdg-netz/nautobot-plugins/nautobot-ui-plugin/',
     # download_url='https://github.com/iDebugAll/nextbox-ui-plugin/archive/v0.9.2.tar.gz',
