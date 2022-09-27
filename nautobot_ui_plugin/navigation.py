83c587c55a2fdf884879ed47915d0297afae1dfc
Oscar Calaf <ocalaf@gmail.com>
2022-09-27 15:56:11 +0200

    v0.10.7 fixing navigation menu

--- nautobot_ui_plugin/navigation.py
+++ nautobot_ui_plugin/navigation.py
@@ -3,8 +3,9 @@ from nautobot.extras.plugins import PluginMenuItem
 
 menu_items = (
     PluginMenuItem(
-        link='plugins:nautobot_ui_plugin:topology',
-        link_text='Topology Viewer',
-        buttons=()
+        link="plugins:nautobot_ui_plugin:topology",
+        link_text="Topology Viewer",
+        permissions=["nautobot_ui_plugin.view_topology"]
+        buttons=(),
     ),
 )
